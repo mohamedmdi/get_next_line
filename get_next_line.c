54ba9e62fc4fc1a7e2b1ca0072b5cc668a17914a
Mohamed Chergui <mchergui@e1r11p12.1337.ma>
2019-11-06 22:57:50 +0100

    updated 2 commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,96 @@
 /*   By: mchergui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 21:08:37 by mchergui          #+#    #+#             */
-/*   Updated: 2019/10/28 21:08:55 by mchergui         ###   ########.fr       */
+/*   Updated: 2019/11/05 18:50:50 by mchergui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *checker(char *remainder, char **line)
-{
-    char *ptr;
-    ptr = NULL;
-    size_t l;
 
-    if (remainder)
-        if ((ptr = ft_strchr(remainder, '\n')))        
-        {
-            *ptr = '\0';
-            *line = ft_strdup(remainder);
-            ptr++;
-            l = strlen(ptr);
-            ft_strlcpy(remainder, ptr, l + 1);
-        }
-        else
-        {
-            *line = ft_strdup(remainder);
-            free(remainder);
-            remainder = NULL;
-        }
-    else
-        *line = ft_strdup("");
-    return (ptr);
+void	ft_strdel(char **as)
+{
+	if (*as != NULL)
+	{
+		free(*as);
+		*as = NULL;
+	}
 }
 
-int get_next_line(int fd, char **line)
+int		checker(char **remainder, char **line, char* buff)
 {
-    static char *rem;
-    char        *buff;
-    int         bwr;
-    char        *ptr;
-    
- 
+	char		*ptr;
+	char		*tmp;
 
-    buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    ptr = checker(rem, line);
-    while (!ptr && (bwr = read(fd, buff, BUFFER_SIZE)))
-    {
-        buff[bwr] = '\0';
-        if ((ptr = ft_strchr(buff, '\n')))  
-        {
-            *ptr = '\0';
-            rem = ft_strdup(ptr + 1);
-        }
-        *line = ft_strjoin(*line ,buff);
-    }
-        free(buff);
-        buff = NULL;
-        if (bwr || ft_strlen(rem))
-            return(1);
-        else
-            return(0);
-        //return(bwr || ft_strlen(rem)) ? 1 : 0;
+	if (*remainder)
+	{
+		if ((ptr = ft_strchr(*remainder, '\n')))
+		{
+			*ptr = '\0';
+			ptr++;
+			tmp = *line;
+			*line = ft_strdup(*remainder);
+			//ft_strdel(&tmp);
+			*remainder = ft_strdup(ptr);
+			ft_strdel(&buff);
+			return (1);
+		}
+		else
+		{	
+			tmp = *line;
+			*line = ft_strdup(*remainder);
+			//ft_strdel(&tmp);
+			ft_strdel(remainder);
+		}
+	}
+	else
+		*line = ft_strdup("");
+	return (0);
 }
 
+int		get_next_line(int fd, char **line)
+{
+	static char	*rem;
+	char		*buff;
+	int			bwr;
+	char		*ptr;
+	char		*tmp;
+
+	if (BUFFER_SIZE < 0 || read(fd, NULL, 0) < 0 || !line 
+	|| !(buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	if(checker(&rem, line, buff))
+		return (1);
+	while ((bwr = read(fd, buff, BUFFER_SIZE)))
+	{
+		buff[bwr] = '\0';
+		if ((ptr = ft_strchr(buff, '\n')) != NULL)
+		{
+			*ptr = '\0';
+			rem = ft_strdup(ptr + 1);
+			*line = ft_strjoin(*line, buff);
+			ft_strdel(&buff);
+			return(1);
+		}
+		tmp = *line;
+		*line = ft_strjoin(*line, buff);
+		ft_strdel(&tmp);
+	}
+	ft_strdel(&buff);
+	return(0);
+}
 int main()
 {
     char *line; 
     int fd;
     
-    fd = open("text.txt", O_RDONLY);
-    int ret = 0;
-        while (get_next_line(fd, &line))
-            printf("%s\n", line);
-            ret++;
-}
+    fd = open("64bit_paragraph.txt", O_RDONLY);
+	int g;
+	g = 1;
+	while (g)
+    {
+		g = get_next_line(fd, &line);
+		printf("%s--%d\n", line, g);
+		free(line);
+	}
+ }
